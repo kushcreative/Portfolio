333dcf919e570474c16ac431077183ac7bb43ffd
Kushal <kushalvisualsdesigner@gmail.com>
2026-08-20 15:52:10 +0530

    Rename Green folder to Studio

--- studio/shared.js
+++ studio/shared.js
@@ -94,7 +94,7 @@
           color: '#3a7336',
         },
       ],
-      projectUrl: '/green/project-nexora',
+      projectUrl: '/studio/project-nexora',
     },
     chronex: {
       title: 'CHRONEX',
@@ -122,7 +122,7 @@
           color: '#284a70',
         },
       ],
-      projectUrl: '/green/project-chronex',
+      projectUrl: '/studio/project-chronex',
     },
   };
 
